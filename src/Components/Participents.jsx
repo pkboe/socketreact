0ced9030440e331a00efde580873ba39cdbe433b
Pranil Kharche <pranilkharche@gmail.com>
2021-01-07 00:13:10 +0530

    Okay

--- src/Components/Participents.jsx
+++ src/Components/Participents.jsx
@@ -1,73 +1,73 @@
-const Participent = () => {
-  return (
-    <div
-      className="Participent"
-      style={{
-        display: "flex",
-        padding: 5,
-        justifyContent: "space-between",
-        borderRadius: 20,
-        alignItems: "center",
-      }}
-    >
-      <div
-        style={{
-          width: "100px",
-          height: "110px",
-          backgroundColor: "grey",
-        }}
-      >
-        IMAGE
-      </div>
-      <div className="ParticipentData">
-        <h4>Mr.Pranil Kharche</h4>
-        <h5>
-          <i>Ai Score : </i> 99%{" "}
-        </h5>
-        <h6></h6>
-        <h6></h6>
-      </div>
-    </div>
-  );
-};
+// const Participent = () => {
+//   return (
+//     <div
+//       className="Participent"
+//       style={{
+//         display: "flex",
+//         padding: 5,
+//         justifyContent: "space-between",
+//         borderRadius: 20,
+//         alignItems: "center",
+//       }}
+//     >
+//       <div
+//         style={{
+//           width: "100px",
+//           height: "110px",
+//           backgroundColor: "grey",
+//         }}
+//       >
+//         IMAGE
+//       </div>
+//       <div className="ParticipentData">
+//         <h4>Mr.Pranil Kharche</h4>
+//         <h5>
+//           <i>Ai Score : </i> 99%{" "}
+//         </h5>
+//         <h6></h6>
+//         <h6></h6>
+//       </div>
+//     </div>
+//   );
+// };
 
-const Participents = (props) => {
-  return (
-    <div
-      className="ParticipentsList"
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        borderRadius: 20,
-        border: "2px solid #240598 ",
-        width: "25%",
-        maxWidth: "300px",
-        minWidth: "270px",
-        padding: 12,
-      }}
-    >
-      <h2
-        style={{
-          color: "white",
-          borderRadius: 20,
-          backgroundColor: "#240598",
-          textAlign: "center",
-          height: "max-content",
-          width: "max-content",
-          paddingLeft: 5,
-          paddingRight: 5,
-        }}
-      >
-        Particiepents
-      </h2>
-      <Participent />
-      <Participent />
-      <Participent />
-      <Participent />
-      <Participent />
-      <Participent />
-    </div>
-  );
-};
+// const Participents = (props) => {
+//   return (
+//     <div
+//       className="ParticipentsList"
+//       style={{
+//         display: "flex",
+//         flexDirection: "column",
+//         borderRadius: 20,
+//         border: "2px solid #240598 ",
+//         width: "25%",
+//         maxWidth: "300px",
+//         minWidth: "270px",
+//         padding: 12,
+//       }}
+//     >
+//       <h2
+//         style={{
+//           color: "white",
+//           borderRadius: 20,
+//           backgroundColor: "#240598",
+//           textAlign: "center",
+//           height: "max-content",
+//           width: "max-content",
+//           paddingLeft: 5,
+//           paddingRight: 5,
+//         }}
+//       >
+//         Particiepents
+//       </h2>
+//       <Participent />
+//       <Participent />
+//       <Participent />
+//       <Participent />
+//       <Participent />
+//       <Participent />
+//     </div>
+//   );
+// };
 
-export default Participents;
+// export default Participents;
